25283b438ba28d51e52d85c070fcceb09875d1af
Brad Wilson <mrbrad8381@gmail.com>
2026-09-06 23:56:05 +1000

    Add Pandemic rare card

---
+++ Brad8381PlagueBringerCode/Cards/Pandemic.cs
@@ -0,0 +1,35 @@
+using BaseLib.Utils;
+using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Extensions;
+using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Cards;
+
+public sealed class Pandemic : PlagueBringerCard, IPlagueCard
+{
+    public override string CustomPortraitPath => "card.png".BigCardImagePath();
+    public override string PortraitPath => "card.png".CardImagePath();
+    public override string BetaPortraitPath => "card.png".CardImagePath();
+
+    public Pandemic() : base(2, CardType.Skill, CardRarity.Rare, TargetType.AllEnemies)
+    {
+        WithVars(new PowerVar<PlaguePower>("Plague", 4));
+        WithKeywords(CardKeyword.Exhaust);
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        if (CombatState == null) return;
+
+        foreach (var enemy in CombatState.HittableEnemies.Where(enemy => enemy.IsAlive).ToArray())
+            await PowerCmd.Apply<PlaguePower>(choiceContext, enemy, DynamicVars["Plague"].IntValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["Plague"].UpgradeValueBy(2m);
+    }
+}
