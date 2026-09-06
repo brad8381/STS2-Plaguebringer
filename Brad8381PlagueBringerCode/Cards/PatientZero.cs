d2d9d3d4dff79d0adbac020f58b224f6b8783cff
Brad Wilson <mrbrad8381@gmail.com>
2026-09-06 23:56:16 +1000

    Add Patient Zero rare card

---
+++ Brad8381PlagueBringerCode/Cards/PatientZero.cs
@@ -0,0 +1,39 @@
+using BaseLib.Utils;
+using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Extensions;
+using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Cards;
+
+public sealed class PatientZero : PlagueBringerCard, IPlagueCard
+{
+    public override string CustomPortraitPath => "card.png".BigCardImagePath();
+    public override string PortraitPath => "card.png".CardImagePath();
+    public override string BetaPortraitPath => "card.png".CardImagePath();
+
+    public PatientZero() : base(1, CardType.Skill, CardRarity.Rare, TargetType.AnyEnemy)
+    {
+        WithVars(new PowerVar<PlaguePower>("Plague", 8));
+        WithKeywords(CardKeyword.Exhaust);
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        if (play.Target is not { IsAlive: true } target) return;
+
+        await PowerCmd.Apply<PlaguePower>(
+            choiceContext,
+            target,
+            DynamicVars["Plague"].IntValue,
+            Owner.Creature,
+            this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["Plague"].UpgradeValueBy(3m);
+    }
+}
