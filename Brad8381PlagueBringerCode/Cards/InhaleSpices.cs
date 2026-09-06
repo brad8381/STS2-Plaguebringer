3660b946d70890b2b5971f06a99f19abed5e159b
Brad Wilson <mrbrad8381@gmail.com>
2026-09-06 23:08:16 +1000

    Fix missing CardModel namespace

--- Brad8381PlagueBringerCode/Cards/InhaleSpices.cs
+++ Brad8381PlagueBringerCode/Cards/InhaleSpices.cs
@@ -3,6 +3,7 @@ using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Cards;
