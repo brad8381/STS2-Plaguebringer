b6c3cd891310776b5b8314cbd175d604e140ae6f
Brad Wilson <mrbrad8381@gmail.com>
2026-09-06 22:31:22 +1000

    Fix Strike CardPlay beta API usage

--- Brad8381PlagueBringerCode/Cards/StrikePlagueBringer.cs
+++ Brad8381PlagueBringerCode/Cards/StrikePlagueBringer.cs
@@ -17,7 +17,7 @@ public sealed class StrikePlagueBringer : PlagueBringerCard
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
-        await CommonActions.CardAttack(this, play.Target).Execute(choiceContext);
+        await CommonActions.CardAttack(this, play).Execute(choiceContext);
     }
 
     protected override void OnUpgrade()
