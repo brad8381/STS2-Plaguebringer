4b9b15521cc1ddcd04ed6156f1239fa07aa853b0
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 18:12:34 +1000

    Add Carrion Cloud card

---
+++ Brad8381PlagueBringerCode/Cards/CarrionCloud.cs
@@ -0,0 +1,30 @@
+using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Cards;
+
+public sealed class CarrionCloud : PlagueBringerCard, IPlagueCard
+{
+    public CarrionCloud() : base(2, CardType.Power, CardRarity.Rare, TargetType.Self)
+    {
+        WithVars(new DynamicVar("SpreadPercent", 50));
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await PowerCmd.Apply<CarrionCloudPower>(
+            choiceContext,
+            Owner.Creature,
+            DynamicVars["SpreadPercent"].IntValue,
+            Owner.Creature,
+            this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["SpreadPercent"].UpgradeValueBy(25m);
+    }
+}
