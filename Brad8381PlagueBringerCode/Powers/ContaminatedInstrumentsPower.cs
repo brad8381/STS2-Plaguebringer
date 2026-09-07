af650077babf0bf3b56833183e201d1241ee82ff
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 18:29:22 +1000

    Fix Contaminated Instruments CardModel namespace

--- Brad8381PlagueBringerCode/Powers/ContaminatedInstrumentsPower.cs
+++ Brad8381PlagueBringerCode/Powers/ContaminatedInstrumentsPower.cs
@@ -2,6 +2,7 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
 
 namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Powers;
 
