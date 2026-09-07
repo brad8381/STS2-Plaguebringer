2f4315947f9c6a98aba486c3e22e2ad5b19e1b1f
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 17:43:05 +1000

    Migrate Airborne Cultures turn-end hook to beta API

--- Brad8381PlagueBringerCode/Powers/AirborneCulturesPower.cs
+++ Brad8381PlagueBringerCode/Powers/AirborneCulturesPower.cs
@@ -1,6 +1,7 @@
 using BaseLib.Abstracts;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 
@@ -18,9 +19,13 @@ public sealed class AirborneCulturesPower : PlagueBringerPower
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
 
-    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    public override async Task AfterSideTurnEnd(
+        PlayerChoiceContext choiceContext,
+        CombatSide side,
+        IEnumerable<Creature> participants)
     {
         if (side != Owner.Side || Owner.CombatState == null) return;
+
         Flash();
         foreach (var enemy in Owner.CombatState.GetOpponentsOf(Owner).Where(enemy => enemy.IsAlive).ToArray())
             await PowerCmd.Apply<PlaguePower>(choiceContext, enemy, Amount, Owner, null);
