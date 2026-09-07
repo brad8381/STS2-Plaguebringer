060c8668640ac810f3b788d5652e3cbc2e8b2cd8
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 12:40:30 +1000

    Use Patient Zero card art

--- Brad8381PlagueBringerCode/Cards/PatientZero.cs
+++ Brad8381PlagueBringerCode/Cards/PatientZero.cs
@@ -1,5 +1,4 @@
 using BaseLib.Utils;
-using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Extensions;
 using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
@@ -10,10 +9,6 @@ namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Cards;
 
 public sealed class PatientZero : PlagueBringerCard, IPlagueCard
 {
-    public override string CustomPortraitPath => "card.png".BigCardImagePath();
-    public override string PortraitPath => "card.png".CardImagePath();
-    public override string BetaPortraitPath => "card.png".CardImagePath();
-
     public PatientZero() : base(1, CardType.Skill, CardRarity.Rare, TargetType.AnyEnemy)
     {
         WithVars(new PowerVar<PlaguePower>("Plague", 8));
