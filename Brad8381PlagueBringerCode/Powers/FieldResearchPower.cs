f88830a0601d6c20935f3bfcdc976b2c9df74146
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 23:06:04 +1000

    Fix CardModel namespace in FieldResearchPower

--- Brad8381PlagueBringerCode/Powers/FieldResearchPower.cs
+++ Brad8381PlagueBringerCode/Powers/FieldResearchPower.cs
@@ -2,6 +2,7 @@ using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Mechanics;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
 
 namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Powers;
 
