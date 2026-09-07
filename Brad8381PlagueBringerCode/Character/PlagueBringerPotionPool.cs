c8b5a3e17e32781c4e485cc01a3b3d6a106153d6
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 11:42:22 +1000

    Use safe vanilla energy icons for Plaguebringer potion pool

--- Brad8381PlagueBringerCode/Character/PlagueBringerPotionPool.cs
+++ Brad8381PlagueBringerCode/Character/PlagueBringerPotionPool.cs
@@ -1,5 +1,4 @@
 using BaseLib.Abstracts;
-using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Extensions;
 using Godot;
 
 namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Character;
@@ -7,8 +6,9 @@ namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Character;
 public class PlagueBringerPotionPool : CustomPotionPoolModel
 {
     public override Color LabOutlineColor => PlagueBringer.Color;
-    
 
-    public override string BigEnergyIconPath => "charui/big_energy.png".ImagePath();
-    public override string TextEnergyIconPath => "charui/text_energy.png".ImagePath();
+    public override string BigEnergyIconPath =>
+        "res://images/atlases/ui_atlas.sprites/card/energy_ironclad.tres";
+    public override string TextEnergyIconPath =>
+        "res://images/atlases/ui_atlas.sprites/card/energy_ironclad.tres";
 }
