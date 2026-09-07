f3eec9366980b355ccb30fa31e44e3faca115451
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 18:39:13 +1000

    Fix Surgical Sweep beta damage APIs

--- Brad8381PlagueBringerCode/Cards/SurgicalSweep.cs
+++ Brad8381PlagueBringerCode/Cards/SurgicalSweep.cs
@@ -26,7 +26,7 @@ public sealed class SurgicalSweep : PlagueBringerCard, IPlagueCard
             .ToArray();
 
         await DamageCmd.Attack(DynamicVars.Damage.IntValue)
-            .FromCard(this)
+            .FromCard(this, play)
             .TargetingAllOpponents(combatState)
             .WithHitFx("vfx/vfx_attack_slash")
             .Execute(choiceContext);
@@ -39,8 +39,8 @@ public sealed class SurgicalSweep : PlagueBringerCard, IPlagueCard
                 enemy,
                 bonus,
                 ValueProp.Unpowered,
-                Owner.Creature,
-                this);
+                this,
+                play);
         }
     }
 
