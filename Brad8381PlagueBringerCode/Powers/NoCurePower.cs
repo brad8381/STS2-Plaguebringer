751ee011a676d975b55ef3cce706b04e76d91a3a
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 18:14:02 +1000

    Add No Cure power

---
+++ Brad8381PlagueBringerCode/Powers/NoCurePower.cs
@@ -0,0 +1,16 @@
+using MegaCrit.Sts2.Core.Entities.Powers;
+
+namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Powers;
+
+public sealed class NoCurePower : PlagueBringerPower
+{
+    public override List<(string, string)>? Localization =>
+        new PowerLoc(
+            "No Cure",
+            "Whenever an enemy's Plague grows, it grows by {Amount} additional Plague.",
+            "Whenever an enemy's Plague grows, it grows by {Amount} additional Plague."
+        );
+
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+}
