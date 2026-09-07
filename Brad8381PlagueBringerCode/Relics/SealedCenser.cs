d4876ffe09eb37f9319f792ad959c6cd73316a81
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 10:35:18 +1000

    Define Sealed Censer localization in code

--- Brad8381PlagueBringerCode/Relics/SealedCenser.cs
+++ Brad8381PlagueBringerCode/Relics/SealedCenser.cs
@@ -1,3 +1,4 @@
+using BaseLib.Abstracts;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Players;
@@ -10,6 +11,13 @@ namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Relics;
 
 public sealed class SealedCenser : PlagueBringerRelic
 {
+    public override List<(string, string)>? Localization =>
+        new RelicLoc(
+            "Sealed Censer",
+            "At the start of combat, apply 1 [gold]Plague[/gold] to ALL enemies.",
+            "The seal was a courtesy."
+        );
+
     public override RelicRarity Rarity => RelicRarity.Starter;
     public override RelicModel GetUpgradeReplacement() => ModelDb.Relic<OpenCenser>();
 
