ae1b06388a7bbb2523a083d52246de453f90dee0
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 23:05:51 +1000

    Fix CardModel namespace in SpecimenActions

--- Brad8381PlagueBringerCode/Mechanics/SpecimenActions.cs
+++ Brad8381PlagueBringerCode/Mechanics/SpecimenActions.cs
@@ -3,6 +3,7 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
 
 namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Mechanics;
 
