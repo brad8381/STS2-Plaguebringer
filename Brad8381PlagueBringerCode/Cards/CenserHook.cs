bcb1de21a50d6b27bc85f0827ac056ba70a3b712
Brad Wilson <mrbrad8381@gmail.com>
2026-09-08 00:25:33 +1000

    Rebalance Censer Hook

--- Brad8381PlagueBringerCode/Cards/CenserHook.cs
+++ Brad8381PlagueBringerCode/Cards/CenserHook.cs
@@ -11,17 +11,27 @@ public sealed class CenserHook : PlagueBringerCard, IPlagueCard
 {
     public CenserHook() : base(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
     {
-        WithDamage(7);
-        WithVars(new PowerVar<PlaguePower>("Plague", 2));
+        WithDamage(5);
+        WithVars(new PowerVar<PlaguePower>("Plague", 4));
     }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
-        if (play.Target is not { IsAlive: true } target) return;
+        if (play.Target is not { IsAlive: true } target)
+            return;
+
         var infected = (target.GetPower<PlaguePower>()?.Amount ?? 0) > 0;
         await CommonActions.CardAttack(this, play).Execute(choiceContext);
+
         if (infected && target.IsAlive)
-            await PowerCmd.Apply<PlaguePower>(choiceContext, target, DynamicVars["Plague"].IntValue, Owner.Creature, this);
+        {
+            await PowerCmd.Apply<PlaguePower>(
+                choiceContext,
+                target,
+                DynamicVars["Plague"].IntValue,
+                Owner.Creature,
+                this);
+        }
     }
 
     protected override void OnUpgrade()
