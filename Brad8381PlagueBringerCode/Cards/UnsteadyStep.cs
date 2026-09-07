5e707ea031f19214ba3751f7180c255ab9ae4998
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 22:37:14 +1000

    Add Sway support common

---
+++ Brad8381PlagueBringerCode/Cards/UnsteadyStep.cs
@@ -0,0 +1,28 @@
+using BaseLib.Utils;
+using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Cards;
+
+public sealed class UnsteadyStep : PlagueBringerCard
+{
+    public UnsteadyStep() : base(1, CardType.Skill, CardRarity.Common, TargetType.AnyEnemy)
+    {
+        WithVars(new PowerVar<SwayPower>("Sway", 2), new CardsVar(1));
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        if (play.Target is not { IsAlive: true } target) return;
+        await PowerCmd.Apply<SwayPower>(choiceContext, target, DynamicVars["Sway"].IntValue, Owner.Creature, this);
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["Sway"].UpgradeValueBy(1m);
+    }
+}
