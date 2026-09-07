5f80f8e2cf0d5a37a7ca0e0b2ef73ebc6d92dfdd
Brad Wilson <mrbrad8381@gmail.com>
2026-09-07 18:10:36 +1000

    Add Reinfection card

---
+++ Brad8381PlagueBringerCode/Cards/Reinfection.cs
@@ -0,0 +1,39 @@
+using Brad8381PlagueBringer.Brad8381PlagueBringerCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Cards;
+
+public sealed class Reinfection : PlagueBringerCard, IPlagueCard
+{
+    public Reinfection() : base(1, CardType.Skill, CardRarity.Common, TargetType.AnyEnemy)
+    {
+        WithVars(new PowerVar<PlaguePower>("Plague", 5));
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        if (play.Target is not { IsAlive: true } target)
+            return;
+
+        if (PlagueCardUtils.GetPlague(target) <= 0)
+        {
+            await PowerCmd.Apply<PlaguePower>(
+                choiceContext,
+                target,
+                DynamicVars["Plague"].IntValue,
+                Owner.Creature,
+                this);
+            return;
+        }
+
+        await PlagueCardUtils.TriggerPlague(choiceContext, target);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["Plague"].UpgradeValueBy(2m);
+    }
+}
