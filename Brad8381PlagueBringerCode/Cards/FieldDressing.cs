48f9fde3c69e0bc6ca4a9a2db74d44b81c5fd5c6
Brad Wilson <mrbrad8381@gmail.com>
2026-09-08 14:53:39 +1000

    Add simple non-Plague block filler

---
+++ Brad8381PlagueBringerCode/Cards/FieldDressing.cs
@@ -0,0 +1,32 @@
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace Brad8381PlagueBringer.Brad8381PlagueBringerCode.Cards;
+
+public sealed class FieldDressing : PlagueBringerCard
+{
+    public FieldDressing() : base(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+    {
+        WithBlock(8);
+    }
+
+    public override List<(string, string)>? Localization =>
+        new CardLoc(
+            "Field Dressing",
+            "Gain {Block:diff()} [gold]Block[/gold]."
+        );
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CommonActions.CardBlock(this, play);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3m);
+    }
+}
