4905574709d6df1648830a27823ae2c047d7a108
Brad Wilson <mrbrad8381@gmail.com>
2026-09-13 01:05:59 +1000

    Fix Contagion Engine power build

--- PlagueBringerCode/Powers/ContagionEnginePower.cs
+++ PlagueBringerCode/Powers/ContagionEnginePower.cs
@@ -1,5 +1,6 @@
 using BaseLib.Abstracts;
 using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 
