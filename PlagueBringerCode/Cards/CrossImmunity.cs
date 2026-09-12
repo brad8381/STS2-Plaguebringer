23c7320111ecc5207f53ad5da822db0d92d6e422
Brad Wilson <mrbrad8381@gmail.com>
2026-09-13 01:21:47 +1000

    Fix Cross Immunity card upgrade variable

--- PlagueBringerCode/Cards/CrossImmunity.cs
+++ PlagueBringerCode/Cards/CrossImmunity.cs
@@ -1,6 +1,7 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using PB.Powers;
 
 namespace PB.Cards;
@@ -10,6 +11,7 @@ public sealed class CrossImmunity : PlagueBringerCard
     public CrossImmunity()
         : base(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
     {
+        WithVars(new DynamicVar("Triggers", 1));
     }
 
     protected override async Task OnPlay(
@@ -19,12 +21,13 @@ public sealed class CrossImmunity : PlagueBringerCard
         await PowerCmd.Apply<CrossImmunityPower>(
             choiceContext,
             Owner.Creature,
-            IsUpgraded ? 2 : 1,
+            DynamicVars["Triggers"].IntValue,
             Owner.Creature,
             this);
     }
 
     protected override void OnUpgrade()
     {
+        DynamicVars["Triggers"].UpgradeValueBy(1m);
     }
 }
